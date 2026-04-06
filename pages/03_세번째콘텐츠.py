ce9720a0ce3da684361d32448158385e7d5eec57
hannah <101084699+nakongssam@users.noreply.github.com>
2026-04-06 16:27:45 +0900

    Update 03_세번째콘텐츠.py

--- pages/03_세번째콘텐츠.py
+++ pages/03_세번째콘텐츠.py
@@ -14,5 +14,5 @@ if st.button('방명록에 추가하기'):
         st.error('모든 값은 필수입니다.')
 
 import pandas as pd
-df_guest = pd.read_csv('./guestbook.csv', encoding='cp949')
+df_guest = pd.read_csv('./guestbook.csv')
 st.write(df_guest)
