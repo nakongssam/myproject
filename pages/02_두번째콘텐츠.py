3f49131d8c31c5d89bc78e4c03f60bb098fc7779
hannah <101084699+nakongssam@users.noreply.github.com>
2026-04-06 16:17:34 +0900

    Update 02_두번째콘텐츠.py

--- pages/02_두번째콘텐츠.py
+++ pages/02_두번째콘텐츠.py
@@ -3,5 +3,5 @@ st.subheader('두번째 콘텐츠')
 import pandas as pd
 df_score = pd.read_csv('./score.csv')
 st.write(df_score)
-df_score['총점'] = df_score.loc[:, '국어']+df_score.loc['영어', :]+df_score.loc['수학', :]
+df_score['총점'] = df_score['국어'] + df_score['영어'] + df_score['수학']
 st.table(df_score)
