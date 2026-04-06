9246e9f0aff8afcaf6a4a1e6444ffe1edc56d7a3
hannah <101084699+nakongssam@users.noreply.github.com>
2026-04-06 16:26:09 +0900

    Update 03_세번째콘텐츠.py

--- pages/03_세번째콘텐츠.py
+++ pages/03_세번째콘텐츠.py
@@ -2,15 +2,14 @@ import streamlit as st
 st.subheader('세번째 콘텐츠')
 
 name = st.text_input('Name:')
-age = st.number_input('Age:', min_value=1, maxvalue=100, step=1)
+age = st.number_input('Age:', min_value=1, max_value=100, step=1)
 email = st.text_input('Email:')
 
 if st.button('방명록에 추가하기'):
-    if all ([name,age,email]):
-        input_data=f'{name},{age},{email}\n'
+    if all([name, age, email]):
+        input_data = f'{name},{age},{email}\n'
         with open('./guestbook.csv', 'a') as f:
             f.write(input_data)
-            f.close()
     else:
         st.error('모든 값은 필수입니다.')
 
